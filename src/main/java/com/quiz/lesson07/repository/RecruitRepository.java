ab30feb98faa9d0790c1e8ce38d610d100cd9662
학원 <test@test.com>
2023-10-24 21:40:57 +0900

    jpa

--- src/main/java/com/quiz/lesson07/repository/RecruitRepository.java
+++ src/main/java/com/quiz/lesson07/repository/RecruitRepository.java
@@ -1,9 +1,10 @@
 package com.quiz.lesson07.repository;
 
-import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.quiz.lesson07.entity.RecruitEntity;
@@ -11,10 +12,22 @@ import com.quiz.lesson07.entity.RecruitEntity;
 @Repository
 public interface RecruitRepository extends JpaRepository<RecruitEntity, Integer>{
 
+	// findById() 는 안 만들어도 됨
 	
+	// JPQL => 엔티티 조회
 	public List<RecruitEntity> findByCompanyId(int companyId);
 	public List<RecruitEntity> findByPositionAndType(String position, String type);
 	public List<RecruitEntity> findByTypeOrSalaryGreaterThanEqual(String type, int salary);
 	public List<RecruitEntity> findTop3ByTypeOrderBySalaryDesc(String type);
-	public List<RecruitEntity> findByTypeDeadlineGreaterThanAndSalaryGreaterThanEqualDesc(Date deadline, int salary, String type);
+	public List<RecruitEntity> findByRegionAndSalaryBetween(String region, int salaryStart, int salaryEnd);
+	
+	// native query 조회 => DB에 직접 조회
+	// 마감일이 2026-04-10 이후이고 연봉이 8100 이상인 정규직 공고를 연봉 내림차순으로 조회하세요.
+	@Query(value = "select * from `recruit` where `deadline` > :deadline and `salary` >= :salary "
+			+ "and `type` = :type order by salary desc", nativeQuery = true)
+	public List<RecruitEntity> findByDeadlineAndSalaryGreaterThanEqualAndTypeOrderBySalaryDesc(
+			@Param("deadline") String deadline,
+			@Param("salary") int salary,
+			@Param("type") String type);
+	
 }
